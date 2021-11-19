7322926114cec0f337dc9c236f924f4cf63f53fc
gabrielgomez20132 <gabrielgomez_100@hotmail.com>
2021-11-19 10:16:50 -0300

    hooks

---
+++ src/HookApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+export const HookApp = () => {
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+        </>
+    )
+}
